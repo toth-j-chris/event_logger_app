a98531c16b33b2c441a18470d75d5b447e175a98
toth-j-chris <toth.j.chris@gmail.com>
2025-03-05 12:24:52 -0500

    fixed outdated import for declarative_base

--- backend/database.py
+++ backend/database.py
@@ -12,8 +12,7 @@ Attributes:
 """
 
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 from dotenv import load_dotenv
 
