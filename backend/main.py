34aad6cd5b49e698a1828e4169fbc1e0ec1db3b6
toth-j-chris <toth.j.chris@gmail.com>
2025-03-05 16:17:34 -0500

    added auth router to main app

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 
+from auth import router as auth_router
+
 app = FastAPI()
+
+app.include_router(auth_router, prefix="/auth", tags=["Authentication"])
 
 @app.get("/")
 async def read_root():
